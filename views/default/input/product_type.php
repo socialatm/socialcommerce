50ca0b70c76ddcf628baee5ad8fbe62b4f405e0d
ray peaslee <github@twentyfiveautumn.com>
2013-09-02 13:04:43 -0400

    code cleanup

--- views/default/input/product_type.php
+++ views/default/input/product_type.php
@@ -5,24 +5,23 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-	 
-	if (isset($vars['class'])) 
-		$class = $vars['class'];
-		
-	if (!$class) $class = "input-product-type";
 	
-	if (!array_key_exists('value', $vars))
-		$vars['value'] = 1;
-
-	$default_produt_types = $CONFIG->produt_type_default;
-	$produt_type_label = elgg_echo('product:type');
+	$class = isset($vars['class'])? $vars['class'] : "input-product-type" ;
+	if (!array_key_exists('value', $vars)) { $vars['value'] = 1; }
+	
+	$default_produt_types = $CONFIG->produt_type_default;			// @todo - spelling issue...
+	$product_type_label = elgg_echo('product:type');
+	
+	//	make sure $vars['value'] is an array...
+	$vars['value'] = is_array($vars['value']) ? $vars['value'] : array($vars['value']);
+	
  	if (is_array($default_produt_types) && sizeof($default_produt_types) > 0) {	 
 ?>
 		<p>
-			<label><span style="color:red">*</span><?php echo $produt_type_label?></label><br />
+			<label><span style="color:red">*</span><?php echo $product_type_label?></label><br />
 			<select <?php if($vars['multiple']) echo $vars['multiple']; ?> id="<?php echo $vars['internalname']; ?>" name="<?php echo $vars['internalname']; ?><?php if($vars['multiple']) echo "[]"; ?>" <?php if (isset($vars['js'])) echo $vars['js']; ?> <?php if ((isset($vars['disabled'])) && ($vars['disabled'])) echo ' disabled="yes" '; ?> class="<?php echo $class; ?>">
 			<?php
 			    foreach($default_produt_types as $option) {
@@ -36,5 +35,5 @@
 			</select>
 		</p>	
 <?php
-	}		
+	}	
 ?>
