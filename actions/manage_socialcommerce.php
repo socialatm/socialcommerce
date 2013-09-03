e1a202e4593d6774a59f242957eb40880e618e2c
ray peaslee <github@twentyfiveautumn.com>
2013-09-03 07:41:22 -0400

    code cleanup

--- actions/manage_socialcommerce.php
+++ actions/manage_socialcommerce.php
@@ -5,12 +5,11 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-
-	global $CONFIG;
-	//admin_gatekeeper();
+	 
+	gatekeeper();
 	load_checkout_actions();
 	load_currency_actions();
 	
