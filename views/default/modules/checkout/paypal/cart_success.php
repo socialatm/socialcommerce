eb64c72622f0e24350f68e895eb95e8b9f7c538c
ray peaslee <github@twentyfiveautumn.com>
2013-09-03 13:59:31 -0400

    spelling corrections

--- views/default/modules/checkout/paypal/cart_success.php
+++ views/default/modules/checkout/paypal/cart_success.php
@@ -5,11 +5,10 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
 	 
-	global $CONFIG;
 	gatekeeper();
 	// Get objects
 	
@@ -17,21 +16,22 @@
 			$method = $_SESSION['CHECKOUT']['checkout_method'];
 			$function = 'makepayment_'.$method;
 				
-			$cart_sucess_load =get_input('cart_sucess_load');
-			$success =get_input('success');
+			$cart_success_load = get_input('cart_success_load');
+			$success = get_input('success');
 			if(function_exists($function)){
-				if($cart_sucess_load!=2){
+				if($cart_success_load !=2 ){
 					$success = $function();
 				}
+				
 				if($success){
-					if($cart_sucess_load!=2){
-						$redirect =  elgg_add_action_tokens_to_url($CONFIG->wwwroot."action/{$CONFIG->pluginname}/manage_socialcommerce?manage_action=cart_success&cart_sucess_load=2&success={$success}");
+					if($cart_success_load != 2 ){
+						$redirect =  elgg_add_action_tokens_to_url($CONFIG->wwwroot."action/socialcommerce/manage_socialcommerce?manage_action=cart_success&cart_success_load=2&success={$success}");
 						forward($redirect);
 						exit();
 					}else{
 						$body = elgg_echo('cart:success:content');
 						$back_text = elgg_echo('checkout:back:text');
-						$action = $CONFIG->wwwroot."pg/{$CONFIG->pluginname}/{$_SESSION['user']->username}/all";
+						$action = $CONFIG->wwwroot."pg/socialcommerce/{$_SESSION['user']->username}/all";
 						$area2 = <<< AREA2
 							<div class="contentWrapper stores">
 								<br>{$body}<br><br>
@@ -42,13 +42,13 @@
 AREA2;
 					}
 				}else{
-					$redirect =  $CONFIG->wwwroot."action/{$CONFIG->pluginname}/manage_socialcommerce?manage_action=checkout_error";
+					$redirect =  $CONFIG->wwwroot."action/socialcommerce/manage_socialcommerce?manage_action=checkout_error";
 					forward($redirect);
 				}
 				echo $area2;
 			}
 			unset($_SESSION['CHECKOUT']);
 		}else{
-			forward($CONFIG->wwwroot."pg/".$CONFIG->pluginname."/".$_SESSION['user']->username."/all");
+			forward($CONFIG->wwwroot."pg/socialcommerce/".$_SESSION['user']->username."/all");
 		}
 ?>
