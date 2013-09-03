5c6f3e2a5a61504999a39a7c196b0fa550ff9bac
ray peaslee <github@twentyfiveautumn.com>
2013-09-03 17:18:02 -0400

    small language update

--- modules/checkout/paypal/languages/en.php
+++ modules/checkout/paypal/languages/en.php
@@ -5,7 +5,7 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
 	 
@@ -19,11 +19,11 @@
 		'display:name' => "Display Name",
 		'paypal:email' => "PayPal Email ID",
 		'stores:paypal' => 'PayPal',
-		'mode' => 'Mode',
 		'stores:sandbox' => 'Sandbox',
 		'not:fill:paypal:settings' => 'You have selected PayPal Website Payments (Standard) for Checkout. To integrate PayPal Website Payments (Standard) into your store you should fill the settings in <B>Checkout</B> tab.',
 		'missing:fields' => 'You have selected PayPal Website Payments (Standard) for Checkout. You are missing the following fields in PayPal Website Payments (Standard). Your should fill the following fields in <B>Checkout</B> tab.<br/>%s',
 		'checkout:paypal:help' => 'This is the one type of checkout method in paypal',
+		'paypal:settings:not:saved' => '%s settings not saved! Please try again.',
 	);
 					
 	add_translation("en",$english);
