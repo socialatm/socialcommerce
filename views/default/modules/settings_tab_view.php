bbb8186b52cb0652eaa348518df33b353e513bd5
ray peaslee <github@twentyfiveautumn.com>
2013-09-04 18:32:20 -0400

    add shipping and currency tabs

--- views/default/modules/settings_tab_view.php
+++ views/default/modules/settings_tab_view.php
@@ -5,7 +5,7 @@
 	 * @package Elgg Commerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
  	**/ 
 	 
@@ -18,9 +18,9 @@
 	<div id="elgg_horizontal_tabbed_nav">
 		<ul>
 			<li <?php if($filter == "settings") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=settings"><?php echo elgg_echo('general:settings:tab'); ?></a></li>
-			<?php if($settings[0]->checkout_methods){ ?>
-				<li <?php if($filter == "checkout") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=checkout"><?php echo elgg_echo('checkout:methods:tab'); ?></a></li>
-			<?php } ?>
+			<li <?php if($filter == "checkout") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=checkout"><?php echo elgg_echo('checkout:methods:tab'); ?></a></li>
+			<li <?php if($filter == "shipping") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=shipping"><?php echo elgg_echo('shipping:methods:tab'); ?></a></li>
+			<li <?php if($filter == "currency") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=currency"><?php echo elgg_echo('currency:tab'); ?></a></li>
 		</ul>
 	</div>
 	<?php echo $base_view; ?>
