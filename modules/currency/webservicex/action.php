db46e137cab86ae672ce1a0adf3958b3683e30fa
ray peaslee <github@twentyfiveautumn.com>
2013-09-05 18:10:53 -0400

    re-write function get_exchange_rate

--- modules/currency/webservicex/action.php
+++ modules/currency/webservicex/action.php
@@ -5,34 +5,31 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-	 
-	function get_exchange_rate($from_code, $to_code){
-		$post_fields = "FromCurrency=" . urlencode(strtoupper($from_code)) . "&ToCurrency=" . urlencode(strtoupper($to_code));
-		$target_url = "http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate";
-		//$rtn = get_response_from_api($target_url, $post_fields,60);
-		
-		if (!$rtn) {
-			return elgg_echo('provider:request:unavailable');
-		}else{
-			
-		}
-		
-		$xml = @simplexml_load_string($rtn);
-		if(!is_object($xml)) {
-			return elgg_echo('provider:request:Invalid:code');
-		}else{
-			
-		}
 
-		if (count($xml) === 0) {
-			return (double)$xml;
-		} else {
-			return (double)$xml[0];
-		}
-	}
-	
-	
+	/*
+	 * accepts 3 character uppercase currency codes
+	 * @todo - need to add validation to the function --- register_error(elgg_echo('provider:request:unavailable'))
+	 *
+	 */
+
+	function get_exchange_rate($from_Currency,$to_Currency ) {
+		$from_Currency = urlencode(strtoupper($from_Currency));
+		$to_Currency = urlencode(strtoupper($to_Currency));
+ 		$url = "http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=".$from_Currency."&ToCurrency=".$to_Currency;
+		$ch = curl_init();
+		$timeout = 0;
+		curl_setopt ($ch, CURLOPT_URL, $url);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch,  CURLOPT_USERAGENT , "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$rawdata = curl_exec($ch);
+		curl_close($ch);
+	    $data = explode('>', $rawdata);
+		$data = explode('<', $data['2']);
+		$var = round($data['0'],2);
+	    return $var;
+	}	
 ?>
