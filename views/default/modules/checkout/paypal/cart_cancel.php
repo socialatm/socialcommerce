2bcce6f404ebd90afd024fd92cc62cbf964ac62e
ray peaslee <github@twentyfiveautumn.com>
2013-09-06 19:34:58 -0400

    code cleanup

--- views/default/modules/checkout/paypal/cart_cancel.php
+++ views/default/modules/checkout/paypal/cart_cancel.php
@@ -5,23 +5,23 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-	global $CONFIG;
+
 	gatekeeper();
 	
  	$body = elgg_echo('cart:cancel:content');
  	$back_text = elgg_echo('checkout:back:text');
-	$action = $CONFIG->wwwroot."pg/{$CONFIG->pluginname}/{$_SESSION['user']->username}/cart";
-	$area2 = <<<EOF
-		{$title}
-		<div class="contentWrapper stores">
-			{$body}
-			<form action="$action" method="post">
-				<input type="submit" name="btn_submit" value="{$back_text}">
+	$action = get_config('url')."pg/socialcommerce/{$_SESSION['user']->username}/cart";
+
+	$area2 = $title
+		.'<div class="contentWrapper stores">'.
+			$body
+			.'<form action="'.$action.'" method="post">
+				<input type="submit" name="btn_submit" value="'.$back_text.'">
 			</form>
-		</div>
-EOF;
+		</div>';
+
 	echo $area2;
 ?>
