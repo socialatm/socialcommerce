6ec8bd8991c5c59388d8d5e3fb2f3d1ed4ee283e
ray peaslee <github@twentyfiveautumn.com>
2013-09-10 10:28:55 -0400

    work on plugin settings and code cleanup

--- views/default/modules/checkout_methods.php
+++ views/default/modules/checkout_methods.php
@@ -5,31 +5,22 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013 / Cubet Technologies 2009-2010
+	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
 	 
-	global $CONFIG;
 	$checkout_methods = get_checkout_methods();
-	$checkoutmethods = "";
+	$splugin_settings = $vars['entity'];
+	$order = get_input('order') ? get_input('order') : 0 ;
+	$selected_checkoutmethods_guid = $splugin_settings->guid;
+	$selected_checkoutmethods = $splugin_settings->checkout_methods;
+	$selected_checkoutmethods = is_array($selected_checkoutmethods) ? $selected_checkoutmethods : array($selected_checkoutmethods);
 	
-	$settings = $vars['entity'];
-	$order = get_input('order');
-	if(!$order)
-		$order = 0;
-	if($settings){
-		$settings = $settings[0];
-		$selected_checkoutmethods_guid = $settings->guid;
-		$selected_checkoutmethods = $settings->checkout_methods;
-		if(!is_array($selected_checkoutmethods)){
-			$selected_checkoutmethods = array($selected_checkoutmethods);
-		}
-	}
 	if($checkout_methods && $selected_checkoutmethods){
 ?>
 		<div>
-			<script type="text/javascript" src="<?php echo $CONFIG->wwwroot; ?>mod/<?php echo $CONFIG->pluginname; ?>/js/chili-1.7.pack.js"></script>
-			<script type="text/javascript" src="<?php echo $CONFIG->wwwroot; ?>mod/<?php echo $CONFIG->pluginname; ?>/js/jquery.accordion.js"></script>
+			<script type="text/javascript" src="<?php echo get_config('url'); ?>mod/socialcommerce/js/chili-1.7.pack.js"></script>
+			<script type="text/javascript" src="<?php echo get_config('url'); ?>mod/socialcommerce/js/jquery.accordion.js"></script>
 			<script type="text/javascript">
 				$(document).ready(function(){
 					jQuery('#list1b').accordion({
@@ -44,7 +35,7 @@
 				$i = 0;
 				foreach ($selected_checkoutmethods as $selected_checkoutmethod){
 					$method = $checkout_methods[$selected_checkoutmethod];
-					$checkout_contents = elgg_view("modules/checkout/{$selected_checkoutmethod}/{$method->view}",array('entity'=>$settings,'method'=>$method,'base'=>$selected_checkoutmethod,'order'=>$i));
+					$checkout_contents = elgg_view('modules/checkout/'.$selected_checkoutmethod.'/'.$method->view, array('entity'=>$splugin_settings,'method'=>$method,'base'=>$selected_checkoutmethod,'order'=>$i));
 ?>
 					<h3>
 						<a>
