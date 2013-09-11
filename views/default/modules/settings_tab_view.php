a0f797703f64ac99be2b626a20c6f5e006f5a18b
ray peaslee <github@twentyfiveautumn.com>
2013-09-11 15:52:48 -0400

    fixes the settings tabs

--- views/default/modules/settings_tab_view.php
+++ views/default/modules/settings_tab_view.php
@@ -17,10 +17,10 @@
 <div class="">
 	<div id="elgg_horizontal_tabbed_nav">
 		<ul>
-			<li <?php if($filter == "settings") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=settings"><?php echo elgg_echo('general:settings:tab'); ?></a></li>
-			<li <?php if($filter == "checkout") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=checkout"><?php echo elgg_echo('checkout:methods:tab'); ?></a></li>
-			<li <?php if($filter == "shipping") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=shipping"><?php echo elgg_echo('shipping:methods:tab'); ?></a></li>
-			<li <?php if($filter == "currency") echo "class='selected'"; ?>><a href="<?php echo $url; ?>?filter=currency"><?php echo elgg_echo('currency:tab'); ?></a></li>
+			<li <?php if($filter == "general") echo "class='selected'"; ?>><a href="<?php echo get_config('url').'pg/socialcommerce/'.$_SESSION['user']->username.'/settings/general">'.elgg_echo('general:settings:tab'); ?></a></li>
+			<li <?php if($filter == "checkout") echo "class='selected'"; ?>><a href="<?php echo get_config('url').'pg/socialcommerce/'.$_SESSION['user']->username.'/settings/checkout">'.elgg_echo('checkout:methods:tab'); ?></a></li>
+			<li <?php if($filter == "shipping") echo "class='selected'"; ?>><a href="<?php echo get_config('url').'pg/socialcommerce/'.$_SESSION['user']->username.'/settings/shipping">'.elgg_echo('shipping:methods:tab'); ?></a></li>
+			<li <?php if($filter == "currency") echo "class='selected'"; ?>><a href="<?php echo get_config('url').'pg/socialcommerce/'.$_SESSION['user']->username.'/settings/currency">'.elgg_echo('currency:tab'); ?></a></li>
 		</ul>
 	</div>
 	<?php echo $base_view; ?>
