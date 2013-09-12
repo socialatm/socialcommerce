b511ac872f36d342c1ca7b5f112fc04237d4a045
ray peaslee <github@twentyfiveautumn.com>
2013-09-12 17:47:47 -0400

    code cleanup

--- views/default/socialcommerce/mainview.php
+++ views/default/socialcommerce/mainview.php
@@ -2,7 +2,7 @@
 	/**
 	 * Elgg view - product mainview
 	 * 
-	* @package Elgg SocialCommerce
+	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
 	 * @copyright twentyfiveautumn.com 2013
