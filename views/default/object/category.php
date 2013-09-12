b1db52171a6f79c0d9400ab5622272438530b297
ray peaslee <github@twentyfiveautumn.com>
2013-09-12 17:45:18 -0400

    code clean up

--- views/default/object/category.php
+++ views/default/object/category.php
@@ -8,33 +8,18 @@
 	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-
-	global $CONFIG;
 	
 	$category = $vars['entity'];
-	if(!isloggedin()){
-		forward("pg/{$CONFIG->pluginname}/" . $_SESSION['user']->username);
-	}
-	$product_guid = $category->getGUID();
+	$product_guid = $category->guid;
 	$title = $category->title;
 	$desc = $category->description;
-	$owner = $vars['entity']->getOwnerEntity();
-	$friendlytime = friendly_time($vars['entity']->time_created);
+	$owner = $category->getOwnerEntity();
+	$friendlytime = friendly_time($category->time_created);
 	
-	//$mime = $category->mimetype;
+//	$mime = $category->mimetype;
 	
 	if (get_context() == "search") {	// Start search listing version 
-		/*$info = "<p> <a href=\"{$category->getURL()}\">{$title}</a></p>";
-		$info .= "<p class=\"owner_timestamp\"><a href=\"{$vars['url']}pg/{$CONFIG->pluginname}/{$owner->username}\">{$owner->name}</a> {$friendlytime}";
-		$numcomments = elgg_count_comments($category);
-		if ($numcomments)
-			$info .= ", <a href=\"{$category->getURL()}\">" . sprintf(elgg_echo("comments")) . " (" . $numcomments . ")</a>";
-		$info .= "</p>";
-		
-		$icon = "<a href=\"{$category->getURL()}\">" . elgg_view("{$CONFIG->pluginname}/icon", array("mimetype" => $mime, 'thumbnail' => $category->thumbnail, 'category_guid' => $product_guid, 'size' => 'small')) . "</a>";
-		
-		echo elgg_view_listing($icon, $info);*/
-		
+				
 ?>
 		<div>
 			<a  class="object_category_string" href="<?php echo $vars['url']; ?>pg/<?php echo $CONFIG->pluginname; ?>/<?php echo $category->guid; ?>/product_cate">
@@ -50,7 +35,7 @@
 	<div class="storesrepo_stores">
 		<div class="storesrepo_icon">
 					<?php 
-						echo elgg_view("{$CONFIG->pluginname}/icon", array("mimetype" => $mime, 'thumbnail' => $category->thumbnail, 'category_guid' => $product_guid)); 
+						echo elgg_view("socialcommerce/icon", array("mimetype" => $mime, 'thumbnail' => $category->thumbnail, 'category_guid' => $product_guid)); 
 						
 					?>					
 		</div>
@@ -67,7 +52,7 @@
 					echo elgg_view("profile/icon",array('entity' => $owner, 'size' => 'tiny'));
 				
 				?>
-				<p class="storesrepo_owner_details"><b><a href="<?php echo $vars['url']; ?>pg/<?php echo $CONFIG->pluginname; ?>/<?php echo $owner->username; ?>"><?php echo $owner->name; ?></a></b><br />
+				<p class="storesrepo_owner_details"><b><a href="<?php echo $vars['url']; ?>pg/socialcommerce/<?php echo $owner->username; ?>"><?php echo $owner->name; ?></a></b><br />
 				<small><?php echo $friendlytime; ?></small></p>
 		</div>
 		</div>
@@ -81,22 +66,19 @@
 	if ($category->canEdit()) {
 ?>
 	<div class="storesrepo_controls">
-				<p>
-					<a href="<?php echo $vars['url']; ?>mod/<?php echo $CONFIG->pluginname; ?>/edit_category.php?category_guid=<?php echo $category->getGUID(); ?>"><?php echo elgg_echo('edit'); ?></a>&nbsp; 
-					<?php 
-						echo elgg_view('output/confirmlink',array(
-						
-							'href' => $vars['url'] . "action/{$CONFIG->pluginname}/delete_category?category=" . $category->getGUID(),
-							'text' => elgg_echo("delete"),
-							'confirm' => elgg_echo("category:delete:confirm"),
-						
-						));  
-					?>
-				</p>
+		<p>
+			<a href="<?php echo $vars['url']; ?>mod/socialcommerce/edit_category.php?category_guid=<?php echo $category->getGUID(); ?>"><?php echo elgg_echo('edit'); ?></a>&nbsp; 
+			<?php 
+				echo elgg_view('output/confirmlink',array(
+					'href' => $vars['url'] . "action/socialcommerce/delete_category?category=" . $category->getGUID(),
+					'text' => elgg_echo("delete"),
+					'confirm' => elgg_echo("category:delete:confirm"),
+				));  
+			?>
+		</p>
 	</div>
 <?php		
 	}
-
 ?>
 	</div>
 </div>
