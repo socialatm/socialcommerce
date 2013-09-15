9f0cccb22d57e4b2993f044224338b518c1b7e75
ray peaslee <github@twentyfiveautumn.com>
2013-09-15 15:42:18 -0400

    add the activate & deactivate pages

---
+++ activate.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * Elgg socialcommerce activate page
+	 * 
+	 * @package Elgg Commerce
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @author twentyfiveautumn.com
+	 * @copyright twentyfiveautumn.com 2013
+	 * @link http://twentyfiveautumn.com/
+ 	 **/
+	 
+	 /*****
+	 @todo - check to be sure directory is named socialcommerce
+	 *****/
+	 
+	/*****
+	@todo - create default settings object
+	*****/
+	 
+?>
---
+++ deactivate.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Elgg socialcommerce deactivate page
+	 * 
+	 * @package Elgg Commerce
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @author twentyfiveautumn.com
+	 * @copyright twentyfiveautumn.com 2013
+	 * @link http://twentyfiveautumn.com/
+ 	 **/
+	 
+	 
+	 
+	 
+?>
