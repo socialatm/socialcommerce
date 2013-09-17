e6e80f222053ac7c3c6832b5a07b854e7a8918ba
ray peaslee <github@twentyfiveautumn.com>
2013-09-17 19:38:58 -0400

    migrate pages to 1.8

--- edit_category.php
+++ edit_category.php
@@ -7,23 +7,28 @@
 	 * @author twentyfiveautumn.com
 	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
-	 **/ 
+	 **/
 
-	require_once(get_config('path').'engine/start.php');
-	global $CONFIG;
 	admin_gatekeeper();
-	
-	// Render the category upload page
+
+	 global $CONFIG;
+	require_once(get_config('path').'engine/start.php');
 	
 	$category = (int) get_input('category_guid');
-	$title = elgg_view_title($title = elgg_echo('category:edit'));
+	$title = elgg_view_title(elgg_echo('category:edit'));
+	
 	if ($category = get_entity($category)) {
 		if ($category->canEdit()) { 
-    		$area2 = "<div class=\"contentWrapper\">".elgg_view("socialcommerce/forms/edit_category",array('entity' => $category))."</div>";
-    		// These for left side menu
-			$area1 .= gettags();
-			$body = elgg_view_layout('two_column_left_sidebar', $area1, $title . $area2);
-			page_draw(elgg_echo("category:edit"), $body);
+    		$content = $title.'<div class="contentWrapper">'.elgg_view("socialcommerce/forms/edit_category", array('entity' => $category)).'</div>';
+    		$sidebar .= gettags();
+			
+			$params = array(
+				'title' => $title,
+				'content' => $content,
+				'sidebar' => $sidebar,
+				);
+			$body = elgg_view_layout('one_sidebar', $params);
+			echo elgg_view_page(elgg_echo('category:edit'), $body);
 		}
 	} else {
 		forward();
