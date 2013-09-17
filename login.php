925aee745ee4a59fbed06ea67f83d288361fa31d
ray peaslee <github@twentyfiveautumn.com>
2013-09-17 19:09:54 -0400

    migrate to 1.8

--- login.php
+++ login.php
@@ -10,18 +10,19 @@
 	 **/ 
 	
 	global $CONFIG;
-	// Load Elgg engine
-		require_once(get_config('path').'engine/start.php');
+	require_once(get_config('path').'engine/start.php');
 
-		$last_forward_from = get_input('forward_url');
+	$last_forward_from = get_input('forward_url');
 		if($last_forward_from){
 			$_SESSION['last_forward_from'] = $last_forward_from;
 		}
-		$area2 = elgg_view("socialcommerce/login");
-		
-	// Create a layout
-		$body = elgg_view_layout('one_column', $area2);
-	
-	// Finally draw the page
-		page_draw(sprintf(elgg_echo("stores:your"), elgg_get_page_owner_entity()->name), $body);
+	$content = elgg_view("socialcommerce/login");
+	$title = sprintf(elgg_echo("stores:your"), elgg_get_page_owner_entity()->name);	
+	$params = array(
+		'title' => $title,
+		'content' => $content,
+		'sidebar' => $sidebar,
+		);
+	$body = elgg_view_layout('one_sidebar', $params);
+	echo elgg_view_page($title, $body);
 ?>
