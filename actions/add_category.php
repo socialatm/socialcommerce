b635d6e9d69e0153ca4962acf1b28749cb00a8a1
ray peaslee <github@twentyfiveautumn.com>
2013-09-19 17:11:28 -0400

    re-write

--- actions/add_category.php
+++ actions/add_category.php
@@ -8,45 +8,34 @@
 	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-
-	global $CONFIG;
-	
+	 
 	// Get variables
 	$title = trim(get_input("categorytitle"));
 	$product_type_id = trim(get_input("product_type_id"));
-	$desc = get_input("categorybody");
-	$container_guid = (int) get_input('container_guid', 0);
-	if (!$container_guid){
-		$container_guid = $_SESSION['user']->getGUID();
-	}
+	$product_type_id = (int) get_input("product_type_id");
+	$description = get_input("categorybody");
+	$container_guid = elgg_get_plugin_from_id('socialcommerce')->guid;
+	$page_user = elgg_get_logged_in_user_entity();
+	
 	//Validation
-	if(empty($title)){
-		$error_field = elgg_echo("title");
-	}
-	if(empty($product_type_id) || $product_type_id <=0){
-		$error_field = elgg_echo("product:type");
-	}
+	if(empty($title)){ $error_field = elgg_echo("title"); }
+	if(empty($product_type_id) || $product_type_id <=0){ $error_field = elgg_echo("product:type");}
 	if(!empty($error_field)){
 		$_SESSION['category']['categorytitle'] = $title;
 		$_SESSION['category']['categorybody'] = $desc;
 		$_SESSION['category']['product_type_id'] = $product_type_id;
+		register_error(sprintf(elgg_echo("product:validation:null"), $error_field));
 		
-		register_error(sprintf(elgg_echo("product:validation:null"),$error_field));
-		$container_user = get_entity($container_guid);
-		$redirect = $CONFIG->wwwroot . "socialcommerce/" . $container_user->username . "/addcategory/";
+		$redirect = $CONFIG->url.'socialcommerce/'.$page_user->username.'/addcategory/';
 	}else{
 		// Extract categories from, save to default social commerce (for now)
 		$category = new ElggObject();
-		
 		$category->access_id = 2;
-		$category->subtype="category";
+		$category->subtype="sc_category";
 		$category->title = $title;
 		$category->product_type_id = $product_type_id;
-		$category->description = $desc;
-		
-		if ($container_guid){
-			$category->container_guid = $container_guid;
-		}
+		$category->description = $description;
+		$category->container_guid = $container_guid;
 		
 		$result = $category->save();
 		
@@ -56,9 +45,7 @@
 		}else{
 			register_error(elgg_echo("category:uploadfailed"));
 		}
-			
-		$container_user = get_entity($container_guid);
-		$redirect = $CONFIG->wwwroot . "socialcommerce/" . $container_user->username . "/category/";
+		$redirect = $CONFIG->url.'socialcommerce/'.$page_user->username.'/category/';
 	}
 	forward($redirect);
 ?>
