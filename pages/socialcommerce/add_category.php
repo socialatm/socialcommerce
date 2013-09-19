7cbd36ae84a61b6bc183473dbabb4936413072be
ray peaslee <github@twentyfiveautumn.com>
2013-09-19 17:19:01 -0400

    code cleanup

--- pages/socialcommerce/add_category.php
+++ pages/socialcommerce/add_category.php
@@ -1,6 +1,6 @@
 <?php
 	/**
-	 * Elgg category - add entry page
+	 * Elgg category - add category page
 	 * 
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
@@ -8,31 +8,26 @@
 	 * @copyright twentyfiveautumn.com 2013
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-	 
+	
 	admin_gatekeeper();
-			
-	// Get the current page's owner
-	$page_owner = elgg_get_page_owner_entity();
-		if ($page_owner === false || is_null($page_owner)) {
-			$page_owner = $_SESSION['user'];
-			elgg_set_page_owner_guid($_SESSION['guid']);
-		}
-	$container_guid = elgg_get_page_owner_guid();
+	$page_owner = elgg_get_logged_in_user_entity();
+	$container_guid = elgg_get_plugin_from_id('socialcommerce')->guid;
+	
 	//set the title
-	$title = elgg_view_title(elgg_echo('stores:addcate'));
+	$title = elgg_view_title(elgg_echo('stores:addcategory'));
 
 	// Get the form
-	$content .= "<div class=\"contentWrapper\">".elgg_view("socialcommerce/forms/edit_category")."</div>";
+	$content .= '<div class="contentWrapper">'.elgg_view('socialcommerce/forms/edit_category').'</div>';
 		
 	// sidebar
 	$sidebar .= gettags();
 		
 	$params = array(
-	'title' => $title,
-	'content' => $content,
-	'sidebar' => $sidebar,
-	);
+		'title' => $title,
+		'content' => $content,
+		'sidebar' => $sidebar,
+		);
 
 	$body = elgg_view_layout('one_sidebar', $params);
-	echo elgg_view_page($title, $body);
+	echo elgg_view_page(elgg_echo('stores:addcategory'), $body);
 ?>
