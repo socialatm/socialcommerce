21256d72de4265bb988477e8d0fa9af7ea15668b
ray peaslee <github@twentyfiveautumn.com>
2013-09-20 21:44:10 -0400

    add default $socialcommerce plugin object

--- activate.php
+++ activate.php
@@ -16,8 +16,47 @@
 		die();
 	}
 	 
-	/*****
-	@todo - create default settings object
+	/*****	default settings object	*****/
+	
+	if(!elgg_get_plugin_from_id('socialcommerce')){
+		$sc_settings = array(
+			'type' => 'object',
+			'subtype' => 'plugin',
+			'owner_guid' => get_config('site_id'),
+			'site_guid' => get_config('site_id')'
+			'container_guid' => get_config('site_id'),
+			'access_id' => '2',
+			'enabled' => 'yes',
+			'title' = 'socialcommerce',
+			'checkout_method' => serialize(array('paypal')),
+			'default_view' => 'list',
+			'river_settings' => serialize(array('product_add', 'product_checkout')),
+			'socialcommerce_paypal_email' => 'your paypal email goes here',
+			'socialcommerce_paypal_environment' => 'sandbox',
+			'payment_name' => 'PayPal Website Payments (Standard)',
+			);
+		$socialcommerce = new ElggObject();
+
+		foreach ($sc_settings as $key => $value) {
+			$socialcommerce->$key = $value;
+		}
+		$socialcommerce->save();
+		unset($sc_settings);
+		
+	}	/*****	end	default settings object	*****/ 
+	
+	/***** 
+	Here are more options from the old plugin that we haven't used yet:
+	
+		fund_withdraw_methods = '';
+		https_allow = 0;
+		https_url_text = '0';
+		share_this = '0';
+		http_proxy_server = "";
+		http_proxy_port = "";
+		http_verify_ssl = "";
+		allow_add_product = "";
+		allow_shipping_method = 1;
+	
 	*****/
-	 
 ?>
