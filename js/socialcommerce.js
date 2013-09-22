9cd491db6741ebde84c7006e0674387dd28f6375
ray peaslee <github@twentyfiveautumn.com>
2013-09-22 10:59:32 -0400

    code cleanup

--- js/socialcommerce.js
+++ js/socialcommerce.js
@@ -1,32 +1,3 @@
-function ShowHelp(img, title, desc){
-	div = document.createElement('div');
-	div.id = 'help';
-	div.style.display = 'inline';
-	div.style.position = 'absolute';
-	div.style.width = '300';
-	div.style.backgroundColor = '#CFF4FF';
-	div.style.border = 'solid 1px #008ACF';
-	div.style.padding = '10px';
-	div.innerHTML = '<span class=help><b>' + title + '<\/b><\/span><br /><div style="padding-left:10; padding-right:5;padding-top:5px;" class=help>' + desc + '<\/div>';
-
-	var parent = img.parentNode;
-	if(img.nextSibling)
-		parent.insertBefore(div, img.nextSibling);
-	else
-		parent.appendChild(div)
-}
-
-function HideHelp(img){
-	if ($("#help").length) {
-		$("#help").css('display','none');
-		setTimeout('RemoveHelp()', 1);
-	}
-}
-
-function RemoveHelp(){
-	$("#help").remove();
-}
-
 function chkform() {
 	var tax_rate = $('#tax_rate').val();
 	if(tax_rate == '')
@@ -65,6 +36,3 @@ function chkforms() {
 	}
 
 }
-
-
-
