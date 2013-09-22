1b80a80b112bcdcd00eeeca354eb59663198f4dc
ray peaslee <github@twentyfiveautumn.com>
2013-09-22 12:01:35 -0400

    jslint

--- js/socialcommerce.js
+++ js/socialcommerce.js
@@ -1,38 +1,25 @@
 function chkform() {
-	var tax_rate = $('#tax_rate').val();
-	if(tax_rate == '')
-	{
-		alert('Enter tax rate');
-		return false;
-	}
-	if (isNaN(tax_rate))
-	{
-		alert('Enter proper tax rate');
-		return false;
-	}
-	else
-	{
-		return true;
-	}
-
+    "use strict";
+    var tax_rate = $('#tax_rate').val();
+    if (tax_rate === '') {
+        alert('Enter tax rate');
+        return false;
+    }
+    if (isNaN(tax_rate)) {
+        alert('Enter proper tax rate');
+        return false;
+    }
 }
 
 function chkforms() {
-	
-	var tax_rate = $('#taxrate').val();
-	if(tax_rate == '')
-	{
-		alert('Enter tax rate');
-		return false;
-	}
-	if (isNaN(tax_rate))
-	{
-		alert('Enter proper tax rate');
-		return false;
-	}
-	else
-	{
-		return true;
-	}
-
+    "use strict";
+    var tax_rate = $('#taxrate').val();
+    if (tax_rate === '') {
+        alert('Enter tax rate');
+        return false;
+    }
+    if (isNaN(tax_rate)) {
+        alert('Enter proper tax rate');
+        return false;
+    }
 }
