183443f8a199ca8d0f7cb6f47c851dd584305849
ray peaslee <github@twentyfiveautumn.com>
2013-09-25 11:50:12 -0400

    code cleanup

--- modules/menu.php
+++ modules/menu.php
@@ -9,44 +9,16 @@
 	 * @link http://twentyfiveautumn.com/
  	**/
 	
-	/*****************************************************************************************
-		Menus
-	******************************************************************************************/
-	
-	require_once('C:/Program Files (x86)/Zend/Apache2/htdocs/krumo/class.krumo.php');
-
-		foreach($CONFIG->product_type_default as $key) {
-		
-	//	echo $key->display_val."< br />";
-	//	echo $key->value."< br />";
+	foreach($CONFIG->product_type_default as $key) {
 		
 		$menu_item = array(
-						'name' => 'new_product_'.$key->display_val,			
-						'text' => sprintf(elgg_echo("product:type:menu"),$key->display_val), 			
-						'href' => get_config('url').'socialcommerce/'. $_SESSION['user']->username .'/add/?type='.$key->value,			
-						'contexts' => array('stores', 'socialcommerce'),	
-	//					'section' => '',		
-	//					'title' => '',			
-	//					'selected' => '',		
-						'parent_name' => 'stores',	
-	//					'link_class' => '',		
-	//					'item_class' => '',		
-						);
-			elgg_register_menu_item('site', $menu_item);
-		
-		
-		
-		
-		
-		}
-		
-	//	echo sprintf(elgg_echo("product:type:menu"),$CONFIG->product_type_default[0]->display_val);
-		
-	//	krumo($CONFIG->product_type_default); die();
-		
-		
-	// ==============================================================================================================		
-	
-	
+			'name' => 'new_product_'.$key->display_val,			
+			'text' => sprintf(elgg_echo("product:type:menu"),$key->display_val), 			
+			'href' => get_config('url').'socialcommerce/'. $_SESSION['user']->username .'/add/'.$key->value.'/',			
+			'contexts' => array('stores', 'socialcommerce'),	
+			'parent_name' => 'stores',	
+			);
+		elgg_register_menu_item('site', $menu_item);
+	}
 ?>
 	
