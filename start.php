8bdecb61475eaf0e6fc8c02c8488f2190adccc08
ray peaslee <github@twentyfiveautumn.com>
2013-09-25 10:30:18 -0400

    move product types to it's own file
    move menu links for product types as well

--- start.php
+++ start.php
@@ -9,7 +9,7 @@
 	 * @link http://twentyfiveautumn.com/
  	**/
 	
-	// Load socialcommerce model
+	// load socialcommerce model
  	require(get_config('pluginspath').'socialcommerce/modules/module.php');
 	
 	// Set Config Values	
@@ -103,30 +103,9 @@
 	 * Note: This is a secondary system:init call and is run at a super low priority to guarantee that it is called after all
 	 * other plugins have initialised.
 	 */
-   	function sc_product_fields_setup(){
-   		global $CONFIG;
-   		//--- Default product types ----//
-   		$default_product_types = array(
-								(object)array('value'=> 1, 'display_val'=> elgg_echo('stores:simple:products'), 'addto_cart'=> 1 ),
-								(object)array('value'=> 2, 'display_val'=> elgg_echo('stores:digital:products'), 'addto_cart'=> 1 ),
-								(object)array('value'=> 3, 'display_val'=> elgg_echo('stores:virtual:products'), 'addto_cart'=> 1 ),
-								);
-		$CONFIG->product_type_default = elgg_trigger_plugin_hook('socialcommerce:product:type', 'stores', NULL, $default_product_types);
-								 
-   		//--- Default fields for digital products ----//
-		$product_fields[2] = array (
-			'upload' => array('field'=>'file','mandatory'=>1,'display'=>1),
-			'price' => array('field'=>'text','mandatory'=>1,'display'=>1)
-		);
-		$CONFIG->product_fields = elgg_trigger_plugin_hook('socialcommerce:fields', 'stores', NULL, $product_fields);
-		
-		//--- Default related product types ----//
-   		$default_related_product_types = array((object)array('value'=>1,'display_val'=>elgg_echo('stores:related:product'),'addto_cart'=>0),
-								      (object)array('value'=>2,'display_val'=>elgg_echo('stores:services'),'addto_cart'=>0)
-								 );
-								 
-		$CONFIG->default_related_product_types = elgg_trigger_plugin_hook('socialcommerce:relatedproduct:type', 'stores', NULL, $default_related_product_types);
-   	}
+	 
+	// load product types
+ 	require(get_config('pluginspath').'socialcommerce/modules/product_types.php');
 		
 	function socialcommerce_pagesetup() {
 		global $CONFIG;
@@ -151,18 +130,21 @@
 
 				if( elgg_is_admin_logged_in() ){
 				
+	// load socialcommerce menu
+ 	require(get_config('pluginspath').'socialcommerce/modules/menu.php');
+				
 					$menu_item = array(
-			'name' => 'new_product',			
-			'text' => elgg_echo('stores:addpost'), 			
-			'href' => get_config('url').'socialcommerce/'. $_SESSION['user']->username .'/add/',			
-			'contexts' => array('stores', 'socialcommerce'),	
-	//		'section' => '',		
-	//		'title' => '',			
-	//		'selected' => '',		
-			'parent_name' => 'stores',	
-	//		'link_class' => '',		
-	//		'item_class' => '',		
-			);
+						'name' => 'new_product',			
+						'text' => elgg_echo('stores:addpost'), 			
+						'href' => get_config('url').'socialcommerce/'. $_SESSION['user']->username .'/add/',			
+						'contexts' => array('stores', 'socialcommerce'),	
+	//					'section' => '',		
+	//					'title' => '',			
+	//					'selected' => '',		
+						'parent_name' => 'stores',	
+	//					'link_class' => '',		
+	//					'item_class' => '',		
+						);
 			elgg_register_menu_item('site', $menu_item);
 			
 			$menu_item = array(
