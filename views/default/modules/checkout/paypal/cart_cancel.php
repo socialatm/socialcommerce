2b3c16c48c00f4eb95de6f2c53466c6313fb9ec2
ray peaslee <github@twentyfiveautumn.com>
2013-09-26 14:34:58 -0400

    updates to checkout process

--- views/default/modules/checkout/paypal/cart_cancel.php
+++ views/default/modules/checkout/paypal/cart_cancel.php
@@ -15,8 +15,8 @@
 		.'<div class="contentWrapper stores">'.
 			elgg_echo('cart:cancel:content')
 			.'<form action="'.get_config('url').'socialcommerce/'.$_SESSION['user']->username.'/cart" method="post">
-				<input type="submit" name="btn_submit" value="'.elgg_echo("checkout:confirm:btn").'">
-			</form><span class="buy_more"> or <a href="'.get_config('url').'socialcommerce/'.$_SESSION['user']->username.'/all">'.elgg_echo('buy:more').'</a></span>
+				<input type="submit" name="btn_submit" value="'.elgg_echo("checkout:confirm:btn").'" class="elgg-button elgg-button-submit">
+			</form><br /><span class="buy_more"> or <a href="'.get_config('url').'socialcommerce/'.$_SESSION['user']->username.'/all">'.elgg_echo('buy:more').'</a></span>
 		</div>';
 
 	echo $content;			//	@todo - look at these links
