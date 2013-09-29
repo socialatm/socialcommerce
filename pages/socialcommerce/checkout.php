f25273c3f194a4b7cf370863f5f4e645391732c0
ray peaslee <github@twentyfiveautumn.com>
2013-09-29 08:04:15 -0400

    work on new checkout

---
+++ pages/socialcommerce/checkout.php
@@ -0,0 +1,73 @@
+﻿<?php
+
+$hello = "HELLO WORLD";
+
+$content = <<<EOF
+
+        <div class="content">
+            <h1>Checkout</h1><br />
+
+            <script>
+                $(function ()
+                {
+                    $("#wizard").steps({
+                        headerTag: "h2",
+                        bodyTag: "section",
+                        transitionEffect: "fade"
+                    });
+                });
+            </script>
+
+            <div id="wizard">
+                <h2>First Step</h2>
+                <section>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut nulla nunc. Maecenas arcu sem, hendrerit a tempor quis, 
+                        sagittis accumsan tellus. In hac habitasse platea dictumst. Donec a semper dui. Nunc eget quam libero. Nam at felis metus. 
+                        Nam tellus dolor, tristique ac tempus nec, iaculis quis nisi. {$hello}</p>
+                </section>
+
+                <h2>Second Step</h2>
+                <section>
+                    <p>Donec mi sapien, hendrerit nec egestas a, rutrum vitae dolor. Nullam venenatis diam ac ligula elementum pellentesque. 
+                        In lobortis sollicitudin felis non eleifend. Morbi tristique tellus est, sed tempor elit. Morbi varius, nulla quis condimentum 
+                        dictum, nisi elit condimentum magna, nec venenatis urna quam in nisi. Integer hendrerit sapien a diam adipiscing consectetur. 
+                        In euismod augue ullamcorper leo dignissim quis elementum arcu porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Vestibulum leo velit, blandit ac tempor nec, ultrices id diam. Donec metus lacus, rhoncus sagittis iaculis nec, malesuada a diam. 
+                        Donec non pulvinar urna. Aliquam id velit lacus.</p>
+                </section>
+
+                <h2>Third Step</h2>
+                <section>
+                    <p>Morbi ornare tellus at elit ultrices id dignissim lorem elementum. Sed eget nisl at justo condimentum dapibus. Fusce eros justo, 
+                        pellentesque non euismod ac, rutrum sed quam. Ut non mi tortor. Vestibulum eleifend varius ullamcorper. Aliquam erat volutpat. 
+                        Donec diam massa, porta vel dictum sit amet, iaculis ac massa. Sed elementum dui commodo lectus sollicitudin in auctor mauris 
+                        venenatis.</p>
+                </section>
+
+                <h2>Forth Step</h2>
+                <section>
+                    <p>Quisque at sem turpis, id sagittis diam. Suspendisse malesuada eros posuere mauris vehicula vulputate. Aliquam sed sem tortor. 
+                        Quisque sed felis ut mauris feugiat iaculis nec ac lectus. Sed consequat vestibulum purus, imperdiet varius est pellentesque vitae. 
+                        Suspendisse consequat cursus eros, vitae tempus enim euismod non. Nullam ut commodo tortor.</p>
+                </section>
+            </div>
+        </div>
+    
+
+EOF;
+
+$content = '<div class="contentWrapper stores">'.$content.'</div>';
+	$sidebar .= elgg_view("socialcommerce/sidebar");
+	$sidebar .= gettags();
+	
+	$params = array(
+		'title' => $title,
+		'content' => $content,
+		'sidebar' => $sidebar,
+		);
+	$body = elgg_view_layout('one_sidebar', $params);
+	elgg_load_css('jquery.steps');  	
+	elgg_load_js('jquery.steps.min');  
+	echo elgg_view_page(elgg_echo("checkout:process"), $body);
+	
+?>
