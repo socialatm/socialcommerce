9ed5ee0cff5c4c985dbae50f271b9d6fe63e9651
ray peaslee <github@twentyfiveautumn.com>
2014-02-23 10:16:55 -0500

    new add to wishlist form

---
+++ views/default/forms/products/add_wishlist.php
@@ -0,0 +1,29 @@
+<?php
+	/**
+	 * Elgg products add to wishlist form
+	 * 
+	 * @package Elgg Commerce
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @author twentyfiveautumn.com
+	 * @copyright twentyfiveautumn.com 2014
+	 * @link http://twentyfiveautumn.com/
+ 	 **/
+	 
+	echo elgg_view('input/hidden', array(
+		'name' => 'product_guid',
+		'id' => 'product_guid',
+		'value' => $vars['product_guid'],
+		'class' => '',
+		'internalname'=>'product_guid',
+		'style' => '',
+		));
+		
+	echo elgg_view('input/submit', array(
+		'name' => 'submit_add_wishlist',
+		'id' => 'submit_add_wishlist',
+		'value' => elgg_echo('add:wishlist'),
+		'class' => 'elgg-button elgg-button-submit',
+		'internalname'=>'last_name',
+		));
+?>
+		
