982fe9be0577a2e31723c6c7e960b6b20afffd22
ray peaslee <github@twentyfiveautumn.com>
2014-03-02 13:24:16 -0500

    code cleanup and corrections
    code cleanup and corrections

--- actions/delete.php
+++ actions/delete.php
@@ -5,33 +5,30 @@
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
 	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013
+	 * @copyright twentyfiveautumn.com 2014
 	 * @link http://twentyfiveautumn.com/
 	 **/ 
-	 
-	global $CONFIG;
-	$guid = (int) get_input('stores');
-	if ($stores = get_entity($guid)) {
-
-		if ($stores->canEdit()) {
-			$stores->status = 0;
-			$context = elgg_get_context();
-			elgg_set_context('delete_product');
-			$delete = $stores->save();
-			elgg_set_context($context);
+	
+	$pageowner_guid = elgg_get_page_owner_guid();
+	$product_guid = (int)$page[2];
+	
+	// be sure user has permission to delete before continuing
+	if(!can_edit_entity( $product_guid, $pageowner_guid )){
+		register_error(elgg_echo("stores:deletefailed"));
+		forward('socialcommerce/'.$_SESSION['user']->username.'/all/');
+	}
+	
+	if ($stores = get_entity($product_guid)) {
+			$delete = delete_entity($product_guid);
 						
 			if (!$delete) {
 				register_error(elgg_echo("stores:deletefailed"));
 			} else {
 				system_message(elgg_echo("stores:deleted"));
 			}
-		} else {
-			$container = $_SESSION['user'];
-			register_error(elgg_echo("stores:deletefailed"));
-		}
+	
 	} else {
 		register_error(elgg_echo("stores:deletefailed"));
 	}
-	forward("socialcommerce/" . $_SESSION['user']->username);
-exit();
+	forward("socialcommerce/".$_SESSION['user']->username.'/all/');
 ?>
