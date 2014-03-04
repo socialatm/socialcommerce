fb5664574118e8aa2d65e07f051a9b00556a1a95
ray peaslee <github@twentyfiveautumn.com>
2014-03-04 16:10:50 -0500

    new buy now view
    new buy now view

---
+++ views/default/socialcommerce/buy_now.php
@@ -0,0 +1,21 @@
+<?php
+
+     /**
+	 * Elgg products buy now view
+	 * 
+	 * @package Elgg products
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @author twentyfiveautumn.com
+	 * @copyright twentyfiveautumn.com 2014
+	 * @link http://twentyfiveautumn.com/
+	 **/ 
+	 
+	$url = elgg_normalize_url('socialcommerce/graphics/paypal.gif');
+	$image = elgg_view('output/img', array(
+				'src' => $url,
+				'alt' => 'Buy Now',
+				'class' => 'elgg-border-plain elgg-transition',
+			));
+			
+	$content = '<div id="buy_now">'.$image.'</div>';
+	echo $content;
