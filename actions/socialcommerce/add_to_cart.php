0d6e54628edce99f014dbb4790c2741bd7fee4cd
ray peaslee <github@twentyfiveautumn.com>
2015-01-31 09:13:49 -0500

    updating the add to cart action

--- actions/socialcommerce/add_to_cart.php
+++ actions/socialcommerce/add_to_cart.php
@@ -10,12 +10,16 @@
 	 * @version elgg 1.9.4
 	 **/ 
 	 
-	$quantity = (get_input("cartquantity")) ? get_input("cartquantity") : 1;
+	gatekeeper();
+	$user = elgg_get_logged_in_user_entity();
+		
 	$product_guid = get_input("product_guid");
-	$container_guid = (int) get_input('customer_guid') ? (int) get_input('customer_guid') : elgg_get_logged_in_user_entity()->guid ;
-	$cart_owner_guid = (int) get_input('customer_guid') ? (int) get_input('customer_guid') : elgg_get_logged_in_user_entity()->guid ;
 	$product = get_entity($product_guid);
+	
+	$quantity = (get_input("cartquantity")) ? get_input("cartquantity") : 1;
+		
 	$product_type_details = sc_get_product_type_from_value($product->product_type_id);
+
 	if($product_type_details->addto_cart != 1){			//	@todo - throw an error message here...	
 		forward($product->getURL());
 	}
@@ -23,18 +27,51 @@
 	// Check the quantity of a product
 	if($product->quantity > 0 || $product->product_type_id == 2){
 		if(($product->quantity >= $quantity && $quantity > 0) || $product->product_type_id == 2){
-			if(elgg_is_logged_in()){
-				// Get carts of a particular user for find that product is already in particular user's cart
+
+				// Get the users shopping cart
 				$carts = elgg_get_entities(array(
 					'type' => 'object',
 					'subtype' => 'cart',
-					'owner_guid' => $cart_owner_guid, 
+					'owner_guid' => $user->guid, 
 					));
 					
 				if($carts){
 					$cart = $carts[0];
+					unset($carts);
 					$cart_guid = $cart->guid;
-					$cart_item = get_stores_from_relationship('cart_item',$cart_guid,'product_id',$product_guid,'object','cart_item',$_SESSION['user']->getGUID());
+										
+					//	get all the items that are in the cart
+					$cart_items = elgg_get_entities_from_relationship(array(
+						'relationship' => 'cart_item',
+						'relationship_guid' => $cart->guid,
+					));
+					
+					// is this product already in the cart?
+					foreach($cart_items as $key => $value){
+						if($value->guid == $product_guid){
+							$in_cart = TRUE;
+						}
+					}
+					
+					switch($in_cart) {
+						case TRUE:
+							echo 'product is already in the cart';
+							break;
+						default:
+							echo 'product is NOT already in the cart';
+					} 
+					
+					
+require_once('C:/Program Files (x86)/Zend/Apache2/htdocs/krumo/class.krumo.php');
+$arr2 = get_defined_vars();
+// krumo($arr2);
+
+
+krumo($in_cart);
+
+die();
+					
+					
 					if($cart_item){
 						if($product->product_type_id == 1){
 							$cart_item = get_entity($cart_item[0]->guid);
@@ -46,12 +83,12 @@
 								}
 							}else{
 								register_error(sprintf(elgg_echo("cart:quantity:less"), $product->title));
-								$return = elgg_get_config('url').'socialcommerce/' . $product->getOwnerEntity()->username . "/buy/" . $product->getGUID() . "/" . $product->title;
+								$return = elgg_get_config('url').'socialcommerce/'.$user->username . "/buy/" . $product->getGUID() . "/" . $product->title;
 							}
 						} else {
 							register_error(elgg_echo("cart:already:added"));
 							$cart_added = true;
-							$return = elgg_get_config('url').'socialcommerce/' . $product->getOwnerEntity()->username . "/buy/" . $product->getGUID() . "/" . $product->title;
+							$return = elgg_get_config('url').'socialcommerce/'.$user->username . "/buy/" . $product->getGUID() . "/" . $product->title;
 						}
 					}else{
 						$cart_item = new ElggObject();
@@ -60,7 +97,7 @@
 						$cart_item->quantity = $quantity;
 						$cart_item->product_id = $product_guid;
 						$cart_item->amount = $product->price;
-						$cart_item->container_guid = $cart_owner_guid;
+						$cart_item->container_guid = $user->guid;
 						
 						$cart_item_guid = $cart_item->save();
 						if($cart_item_guid){
@@ -72,7 +109,7 @@
 					$cart = new ElggObject();
 					$cart->access_id = 2;
 					$cart->subtype = "cart";
-					$cart->container_guid = $cart_owner_guid;
+					$cart->container_guid = $user->guid;
 					
 					$cart_guid = $cart->save();
 					if($cart_guid){
@@ -83,7 +120,7 @@
 						$cart_item->quantity = $quantity;
 						$cart_item->product_id = $product_guid;
 						$cart_item->amount = $product->price;
-						$cart_item->container_guid = $cart_owner_guid;
+						$cart_item->container_guid = $user->guid;
 						
 						$cart_item_guid = $cart_item->save();
 						if($cart_item_guid){
@@ -92,17 +129,15 @@
 						}
 					}
 				}
-			}
 			if(!$cart_added){
 				if ($result){
 					system_message(elgg_echo("cart:added"));
-					$return = elgg_get_config('url').'socialcommerce/' . $_SESSION['user']->username . "/cart/";
+					$return = elgg_get_config('url').'socialcommerce/'.$user->username.'/cart/';
 				}else {
 					register_error(elgg_echo("cart:addfailed"));
-					$return = elgg_get_config('url').'socialcommerce/' . $product->getOwnerEntity()->username . "/buy/" . $product->getGUID() . "/" . $product->title;
+					$return = elgg_get_config('url').'socialcommerce/'.$user->username.'/buy/'.$product->getGUID().'/'.$product->title;
 				}	
 			}
-			$container_user = get_entity($cart_owner_guid);
 		}else{
 			register_error(elgg_echo("cart:addfailed:quantity"));
 			$return = elgg_get_config('url').'socialcommerce/' . $product->getOwnerEntity()->username . "/buy/" . $product->getGUID() . "/" . $product->title;
