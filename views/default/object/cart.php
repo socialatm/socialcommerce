72802e661caf1849d5892706a849d5cc0ee2dd7f
ray peaslee <github@twentyfiveautumn.com>
2015-01-30 23:17:07 -0500

    almost a total re-write

--- views/default/object/cart.php
+++ views/default/object/cart.php
@@ -9,10 +9,8 @@
 	 * @link http://twentyfiveautumn.com/
 	 * @version elgg 1.9.4
 	 **/ 
-
+	 
 	$cart = $vars['entity'];
-	
-	$cart_guid = $cart->getGUID();
 	$title = $cart->title;
 	$desc = $cart->description;
 	$quantity = $cart->quantity;
@@ -25,44 +23,16 @@
 	$owner = $vars['entity']->getOwnerEntity();
 	$friendlytime = elgg_view_friendly_time($vars['entity']->time_created);
 	
-	if (elgg_get_context() == "search") {	// Start search listing version 
-		$info = "<p> <a href=\"{$product_url}\">{$title}</a></p>";
-		$info .= "<p class=\"owner_timestamp\">{$friendlytime}";
-		$info .= "</p>";
+		$info = '<p> <a href="'.$product_url.'">'.$title.'</a></p>';
+		$info .= '<p class="owner_timestamp">'.$friendlytime.'</p>';
 		$info .= elgg_cart_quantity($cart);
-		$info .= "<div class=\"stores_remove\">".elgg_view('output/confirmlink',array(
-							'href' => elgg_get_config('url'). "action/socialcommerce/remove_cart?cart_guid=" . $cart->getGUID(),
+		
+		$info .= '<div class="stores_remove">'.elgg_view('output/confirmlink',array(
+							'href' => elgg_get_config('url'). "action/socialcommerce/remove_cart?cart_guid=" . $cart->guid,
 							'text' => elgg_echo("remove"),
 							'confirm' => elgg_echo("cart:delete:confirm")
 						))."</div>"; 
-		$icon = elgg_view("socialcommerce/image", array(
-												'entity' => $product,
-												'size' => 'small',
-											  ));
-		
-		echo elgg_view('page/components/image_block', array('image' => $icon, 'body' => $info));
-	}elseif (elgg_get_context() == "confirm") {
-		$info = "<p> <a href=\"{$product_url}\">{$title}</a></p>";
-		$info .= "<p class=\"owner_timestamp\">{$friendlytime}";
-		$info .= "</p>";
-		$info .= elgg_cart_quantity($cart);
-		
-		$icon = elgg_view("socialcommerce/image", array(
-												'entity' => $product,
-												'size' => 'small',
-											  ));
-											  
-		echo elgg_view('page/components/image_block', array('image' => $icon, 'body' => $info));
-	}elseif (elgg_get_context() == "order") {	// Start search listing version 
-		$info = "<p> <a href=\"{$product_url}\">{$title}</a></p>";
-		$info .= "<p class=\"owner_timestamp\">{$friendlytime}";
-		$info .= "</p>";
-		$info .= elgg_cart_quantity($cart);
-		
-		$icon = elgg_view("socialcommerce/image", array(
-												'entity' => $product,
-												'size' => 'small',
-											  ));
-		
-		echo elgg_view('page/components/image_block', array('image' => $icon, 'body' => $info));
-	}
+						
+		$product_image_guid = sc_product_image_guid($cart->product_id);
+		$image = '<img src ="'.elgg_get_config('url').'socialcommerce/'.elgg_get_logged_in_user_entity()->username.'/image/'.$product_image_guid.'/'.'medium'.'"/>'; 				
+		echo elgg_view('page/components/image_block', array('image' => $mage, 'body' => $info));
