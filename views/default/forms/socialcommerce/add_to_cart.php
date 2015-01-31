6abe54428b5aed4034198c713fbbc32e8d70fb61
ray peaslee <github@twentyfiveautumn.com>
2015-01-31 17:56:20 -0500

    new add to cart form - a work in progress

---
+++ views/default/forms/socialcommerce/add_to_cart.php
@@ -0,0 +1,55 @@
+<?php
+	/**
+	 * Elgg products add to cart form
+	 * 
+	 * @package Elgg Commerce
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @author ray peaslee
+	 * @copyright twentyfiveautumn.com 2015
+	 * @link http://twentyfiveautumn.com/
+	 * @version elgg 1.9.4
+ 	 **/
+	
+// echo '<b>'.__FILE__ .' at '.__LINE__; die();
+
+require_once('C:/Program Files (x86)/Zend/Apache2/htdocs/krumo/class.krumo.php');
+// $arr2 = get_defined_vars();
+// krumo($arr2);
+
+// die();
+
+
+
+
+
+
+?>
+
+	<label for="quantity"><?php echo elgg_echo('quantity'); ?>:</label>
+			<?php echo elgg_view('input/text', array(
+					'name' => 'quantity',
+					'id' => 'quantity',
+					'value' => $first_name,
+					'class' => '',
+					'style' => '',
+					));
+			?>
+<?php
+	
+	echo elgg_view('input/hidden', array(
+		'name' => 'product_guid',
+		'id' => 'product_guid',
+		'value' => $vars['product_guid'],
+		'class' => '',
+		'name'=>'product_guid',
+		'style' => '',
+		));
+		
+	echo elgg_view('input/submit', array(
+		'name' => 'submit_add_to_cart',
+		'id' => 'submit_add_to_cart',
+		'value' => elgg_echo('add:cart'),
+		'class' => 'elgg-button-action',
+		));
+?>
+		
