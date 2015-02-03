780841fbd87fa5cb48e7077029ef3f9b06becf3a
ray peaslee <github@twentyfiveautumn.com>
2015-02-02 21:17:02 -0500

    code cleanup

--- views/default/socialcommerce/forms/confirm_cart_list.php
+++ views/default/socialcommerce/forms/confirm_cart_list.php
@@ -4,26 +4,19 @@
 	 * 
 	 * @package Elgg products
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
-	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2014
+	 * @author ray peaslee
+	 * @copyright twentyfiveautumn.com 2015
 	 * @link http://twentyfiveautumn.com/
+	 * @version elgg 1.9.4
 	 **/ 
 	
+	gatekeeper();
 	$user = elgg_get_logged_in_user_entity();
-	 
-	if(elgg_is_logged_in()){
-		if($vars['not_allow'] == 1){
-			$hidden = '<input type="hidden" name="not_allow" value="1">';
-			$action = "#";
-		}else{
-			$action = elgg_get_config('url').'socialcommerce/'.$user->username.'/checkout/';
-		}
-		$username = "/".$user->username;
-	}
+	$action = elgg_get_config('url').'socialcommerce/'.$user->username.'/checkout/';
 	$submit_input = elgg_view('input/submit', array('name' => 'submit', 'value' => elgg_echo('check:out')));
 	$buy_more = elgg_echo('buy:more');
 	$hidden_values = elgg_view('input/securitytoken');
-	$buy_more_link = elgg_get_config('url').'socialcommerce'.$username."/all";
+	$buy_more_link = elgg_get_config('url').'socialcommerce/'.$user->username.'/all';
 
 $form_body = <<< BOTTOM
 		<form method="post" id="checkout_form" action="$action" >
@@ -37,5 +30,4 @@ $form_body = <<< BOTTOM
 			</div>
 		</form>
 BOTTOM;
-
 echo $form_body;
