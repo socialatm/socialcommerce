d1a0a5dc3c4cd334f79c47a8786216021467d0da
ray peaslee <github@twentyfiveautumn.com>
2015-02-06 17:08:34 -0500

    code cleanup

--- pages/socialcommerce/add_category.php
+++ pages/socialcommerce/add_category.php
@@ -7,22 +7,14 @@
 	 * @author ray peaslee
 	 * @copyright twentyfiveautumn.com 2015
 	 * @link http://twentyfiveautumn.com/
+	 * @version elgg 1.9.4
 	 **/ 
 	
-	/***** @link	http://localhost/elgg-1.8.16/socialcommerce/rainman/edit_category/132	*****/
-	/* @todo change this from admin_gatekeeper to if pageowner is entity owner they can edit exisiting category
-		or check if page owner has permission to add a new category */
-	admin_gatekeeper();
 	$category = get_entity($page[2]);
-	$subtype = get_subtype_from_id($category->subtype) ;
-	$options = $subtype === 'sc_category' ? $category : '';
-
-	// set the title
-	$title = $category == FALSE ?  'stores:addcategory':'category:edit';
+	$title = isset($category) ? 'category:edit' : 'stores:addcategory';
 	$title = elgg_view_title(elgg_echo($title));			
 
-	// Get the form
-	$content = elgg_view_form('socialcommerce/category/save', '', array($options) );
+	$content = elgg_view_form('socialcommerce/category/save', '', array('entity' => $category) );
 	
 	$sidebar .= elgg_view("socialcommerce/sidebar");
 	$sidebar .= gettags();
@@ -34,5 +26,4 @@
 		);
 
 	$body = elgg_view_layout('one_sidebar', $params);
-	echo elgg_view_page(elgg_echo($ptitle), $body);
-?>
+	echo elgg_view_page(elgg_echo($title), $body);
