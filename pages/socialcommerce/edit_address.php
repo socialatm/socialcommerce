a08aaf8c863bafaf98889ffd0ff1affa65c6ced6
ray peaslee <github@twentyfiveautumn.com>
2015-02-07 12:10:09 -0500

    add the edit address logic

--- pages/socialcommerce/edit_address.php
+++ pages/socialcommerce/edit_address.php
@@ -4,21 +4,21 @@
 	 * 
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
-	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2014
+	 * @author ray peaslee
+	 * @copyright twentyfiveautumn.com 2015
 	 * @link http://twentyfiveautumn.com/
+	 * @version elgg 1.9.4
 	 **/ 
 	 
 	gatekeeper();
-
 	$address_guid = (int)$page[2];
 	$title = elgg_view_title($title = elgg_echo('address:edit'));
 	
 	if ($address = get_entity($address_guid)) {
 	
 		if ($address->canEdit()) { 
-    		$content = '<div class="contentWrapper">'.elgg_view("socialcommerce/forms/edit_address", array('entity' => $address)).'</div>';
-    		$sidebar .= elgg_view("socialcommerce/sidebar");
+    		$content = '<div class="contentWrapper">'.elgg_view("socialcommerce/forms/checkout_edit_address", array('entity' => $address)).'</div>';
+			$sidebar .= elgg_view("socialcommerce/sidebar");
 			$sidebar .= gettags();
 			
 			$params = array(
@@ -32,4 +32,3 @@
 	} else {
 		forward();
 	}
-?>
