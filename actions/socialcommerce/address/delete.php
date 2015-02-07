877e165d75eaeaacc3b4cdc3746dec9f94929521
ray peaslee <github@twentyfiveautumn.com>
2015-02-07 14:04:51 -0500

    delete address action working

--- actions/socialcommerce/address/delete.php
+++ actions/socialcommerce/address/delete.php
@@ -4,43 +4,26 @@
 	 * 
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
-	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2013
+	 * @author ray peaslee
+	 * @copyright twentyfiveautumn.com 2015
 	 * @link http://twentyfiveautumn.com/
+	 * @version elgg 1.9.4
 	 **/ 
 	 
- 	$guid = (int) get_input('a_id');
-	$user_guid = (int) get_input('u_guid');
-	$ajax = get_input('ajax');
-	if ($address = get_entity($guid)) {
+	$user = elgg_get_logged_in_user_entity();
+	$address_guid = get_input('address_guid');
+	 
+ 	if ($address = get_entity($address_guid)) {
 		if ($address->canEdit()) {
 			if (!$address->delete()) {
-				if(!$ajax){
-					register_error(elgg_echo("address:deletefailed"));
-				}else{
-					echo elgg_echo("address:deletefailed");
-				}
-			} else {
-				if(!$ajax){
-					system_message(elgg_echo("address:deleted"));
-				}else{
-					echo 1;
-				}
-			}
-		} else {
-			$container = $_SESSION['user'];
-			if(!$ajax){
 				register_error(elgg_echo("address:deletefailed"));
 			}else{
-				echo elgg_echo("address:deletefailed");
+				system_message(elgg_echo("address:deleted"));
 			}
-		}
-	} else {
-		if(!$ajax){
-			register_error(elgg_echo("address:deletefailed"));
 		}else{
-			echo elgg_echo("address:deletefailed");
+			register_error(elgg_echo("address:deletefailed"));
 		}
+	}else{
+		register_error(elgg_echo("address:deletefailed"));
 	}
-	if(!$ajax){ forward('socialcommerce/' . $_SESSION['user']->username . "/category/");}
-?>
+forward('socialcommerce/'.$user->username.'/address/');
