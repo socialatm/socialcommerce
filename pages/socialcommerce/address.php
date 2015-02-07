5e8e60c9010d09138ae7bacb330d3bd70dde481d
ray peaslee <github@twentyfiveautumn.com>
2015-02-07 08:41:50 -0500

    Update to use the new address form

--- pages/socialcommerce/address.php
+++ pages/socialcommerce/address.php
@@ -4,49 +4,41 @@
 	 * 
 	 * @package Elgg SocialCommerce
 	 * @license http://www.gnu.org/licenses/gpl-2.0.html
-	 * @author twentyfiveautumn.com
-	 * @copyright twentyfiveautumn.com 2014
+	 * @author ray peaslee
+	 * @copyright twentyfiveautumn.com 2015
 	 * @link http://twentyfiveautumn.com/
+	 * @version elgg 1.9.4
 	 **/ 
 	 
 	gatekeeper();
-	$page_owner = elgg_get_page_owner_entity();
-	$container_guid = elgg_get_page_owner_guid();
-		
+	$user = elgg_get_logged_in_user_entity();
 	$title = elgg_view_title(elgg_echo('stores:address'));
 		
-	// Get address objects
-	if(	elgg_get_entities(array( 	
+	// If the user has addresses show them. If not show the add new address form
+	if(	elgg_get_entities(array('type' => 'object', 'subtype' => 'address', 'owner_guid' => $user->guid ))) {
+		elgg_set_context('search');
+			
+		$content = elgg_list_entities(array(
 			'type' => 'object',
 			'subtype' => 'address',
-			'owner_guid' => $page_owner->guid,
-			)) 
-	){
-			
-	elgg_set_context('search');
-			
-			$content = elgg_list_entities(array(
-						'type' => 'object',
-						'subtype' => 'address',
-						'owner_guid' => $page_owner->guid,
-						'limit' => 10,
-						));
+			'owner_guid' => $user->guid,
+			'limit' => 10,
+			));
 						
 		elgg_set_context('address');
 	}else{
-			$content = elgg_view("socialcommerce/forms/edit_address");
-		}
+		$content = elgg_view_form('socialcommerce/address/save');
+	}
 		
 	$content = '<div class="contentWrapper stores">'.$content.'</div>';
 	$sidebar .= elgg_view("socialcommerce/sidebar");
 	$sidebar .= gettags();
 		
 	$params = array(
-	'title' => $title,
-	'content' => $content,
-	'sidebar' => $sidebar,
+		'title' => $title,
+		'content' => $content,
+		'sidebar' => $sidebar,
 	);
 
 	$body = elgg_view_layout('one_sidebar', $params);
 	echo elgg_view_page($title, $body);
-?>
