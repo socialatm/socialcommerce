5188953ea0afed005a67f9a42aa22b8495e538f5
ray peaslee <github@twentyfiveautumn.com>
2015-02-07 17:38:03 -0500

    Add New Address link added to Address Book title

--- pages/socialcommerce/address.php
+++ pages/socialcommerce/address.php
@@ -13,29 +13,41 @@
 	gatekeeper();
 	$user = elgg_get_logged_in_user_entity();
 	$title = elgg_echo('stores:address');
+	
+	$new_address_link = elgg_view('output/url', array(
+		'href' => 'socialcommerce/'.$user->username.'/address/new',
+		'text' => elgg_echo('add:new:address'),
+		'class' => 'right'
+		));
 		
-	// If the user has addresses show them. If not show the add new address form
-	if(	elgg_get_entities(array('type' => 'object', 'subtype' => 'address', 'owner_guid' => $user->guid ))) {
-		elgg_set_context('search');
+	switch($page[2]) {
+		case "new":
+			$content = elgg_view('socialcommerce/forms/checkout_edit_address');
+			break;
+		default:
+			// If the user has addresses show them. If not show the add new address form
+			if(	elgg_get_entities(array('type' => 'object', 'subtype' => 'address', 'owner_guid' => $user->guid ))) {
+				elgg_set_context('search');
 			
-		$content = elgg_list_entities(array(
-			'type' => 'object',
-			'subtype' => 'address',
-			'owner_guid' => $user->guid,
-			'limit' => 10
-			));
+				$content = elgg_list_entities(array(
+					'type' => 'object',
+					'subtype' => 'address',
+					'owner_guid' => $user->guid,
+					'limit' => 10
+					));
 						
-		elgg_set_context('address');
-	}else{
-		$content = elgg_view('socialcommerce/forms/checkout_edit_address');
-	}
-		
+				elgg_set_context('address');
+			}else{
+				$content = elgg_view('socialcommerce/forms/checkout_edit_address');
+			}
+	}	
+			
 	$content = '<div class="contentWrapper stores">'.$content.'</div>';
 	$sidebar .= elgg_view("socialcommerce/sidebar");
 	$sidebar .= gettags();
 		
 	$params = array(
-		'title' => elgg_view_title($title),
+		'title' => elgg_view_title($title.$new_address_link),
 		'content' => $content,
 		'sidebar' => $sidebar,
 	);
